38b506cd0d932f8f04497dcc1f7d55825effa0a4
Dhanush Arun <71203387+DhanushArunMahasenan@users.noreply.github.com>
2023-04-23 21:10:41 +0000

    Added more functions to main.py

--- main.py
+++ main.py
@@ -1,41 +1,54 @@
 import requests
 import time
 
+#Will create a new record file if it does not already exist
+file = open("record.txt", "a")
+file.close()
 
+#Required for API
 url = 'https://rest.coinapi.io/v1/exchangerate/ETH/USD'
 headers = {'X-CoinAPI-Key' : 'BB37B983-7254-4CA5-BC64-138C4F91ED86'}
+
 
 class Trader:
     
     def __init__(self):
-        self.balance = 100000
-        self.portfolio = [{"name" : "BTC", "quantity": 0, "average price": 0, "selling price": 0, "profit": 0, "balance": 100000}, 
-                    {"name" : "ETH", "quantity": 0, "average price": 0, "selling price": 0, "profit": 0}]
+        if open("record.txt", "r").read() == "":
+            self.portfolio = {"balance" : 100000,
+            "crypto" : [{"name" : "BTC", "quantity": 0, "average price": 0, "selling price": 0, "profit": 0}, 
+                    {"name" : "ETH", "quantity": 0, "average price": 0, "selling price": 0, "profit": 0}]}
+        else:
+            self.portfolio = dict(open("record.txt", "r").read())
     
+    def updatePortfolio(self, latestPortfolio):
+        currentPortfolio = open("record.txt", "w")
+        currentPortfolio.write(str(latestPortfolio))
+        currentPortfolio.close()
+
+        self.portfolio = latestPortfolio
+
+
     def getExchangeRate(self, cryptoName):
         url = f'https://rest.coinapi.io/v1/exchangerate/{cryptoName}/USD'
         response = requests.get(url, headers=headers)
         data = response.json()
-        
-        print(data)
-
         rate = round(data["rate"], 2)
+
         return rate
 
     def buyCrypto(self, name, quantity):
         currentPrice = self.getExchangeRate(name)
+        print(self.getCurrentPortfolio()["balance"])
         
-        if currentPrice * quantity <= self.balance:
-            self.balance -= currentPrice * quantity
+        if currentPrice * quantity <= self.getCurrentPortfolio()["balance"]:
+            self.getCurrentPortfolio()["balance"] -= currentPrice * quantity
         
         else:
             print("Error: Insufficent funds for transaction")
+
+        latestPortfolio = {"balance": self.getCurrentPortfolio()["balance"] - currentPrice * quantity, "crypto": [{"name" : name, "quantity": quantity, "average price": 0, "selling price": 0, "profit": 0}]}
         
-        for i in self.getCurrentPortfolio():
-            if i["name"] == name:
-                i["quantity"] = quantity
-                i["average price"] = currentPrice
-                i["balance"] = self.getBalance() - currentPrice * quantity
+        self.updatePortfolio(latestPortfolio)
                 
         return self.portfolio
 
@@ -57,29 +70,18 @@ class Trader:
         print(self.portfolio) 
     
     def getCurrentPortfolio(self):
-        try:
-            portfolio = open("record.txt", "r")
-            currentPortfolio = portfolio.read()
-            #currentPortfolio = dict(currentPortfolio)
-            return currentPortfolio
-        
-        except Exception as e:
-            print(e)
-            print("No historial transactions found.")
+        portfolio = open("record.txt", "r")
+        currentPortfolio = portfolio.read()
+        if currentPortfolio == "":
             return self.portfolio
-    
-    def getBalance(self):
-        portfolio = self.getCurrentPortfolio()
-        portfolio = dict(portfolio)
-        
-        balance = portfolio[0]["balance"]
-        
-        return balance
+        #currentPortfolio = dict(currentPortfolio)
+        return currentPortfolio
 
 test = Trader()
 
 while True:
-    option = input("Enter 1 to see exchange rates, enter 2 to buy cryptocurrency, 3 to sell cryptocurrency, 4 to view your portfolio, and enter 5 to exit the program.")
+    print("\n-----Menu-----")
+    option = input("Enter 1 to see exchange rates\nEnter 2 to buy cryptocurrency\nEnter 3 to sell cryptocurrency\nEnter 4 to view your portfolio\nEnter 5 to exit the program.")
     
     if option == "1":
         name = input("Enter then name of the cryptocurrency to get the exchange rate: ")
@@ -90,9 +92,6 @@ while True:
         
         buyQuantity = int(input(f"How many {cryptoToBuy} would you like to purchase: "))
         portfolio = test.buyCrypto(cryptoToBuy, buyQuantity)
-        file = open("record.txt", "w")
-        file.write(str(portfolio))
-        file.close()
 
     if option == "3":
         cryptoToSell = input("Would you like to sell bitcoin or ethereum: ")
